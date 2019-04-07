a82638426867a2b167abc01552a2831d5318613e
gsej <gsej@erskinejones.com>
2019-04-07 18:10:07 +0100

    moved Open-Solution to its own file and handle missing solution file properly

---
+++ src/OpenSolution.ps1
@@ -0,0 +1,37 @@
+ <#
+ .Synopsis
+  Opens the first visual studio solution found in the current directory.
+
+ .Description
+  When executed in a directory containing a visual studio solution, launches that solution using the default action for .sln files.
+
+ .Example
+   # Open the solution located in the current directory using the full command.
+   Open-Solution
+ .Example
+   # Open the solution located in the current directory using the alias.
+   sln
+#>
+function Open-Solution
+{
+    Set-StrictMode -Version 2.0
+	$currentDirectory = Get-Location
+  
+    $numberOfSolutionFiles = Get-ChildItem -Name | Where-Object { $_ -match '.sln$' } | Measure-Object | %{$_.Count}  
+   
+    
+    if ($numberOfSolutionFiles -eq 0) 
+    {
+        Write-Host 'No solution file found'
+    }
+    elseif ($numberOfSolutionFiles -eq 1) 
+    {
+        $solutionFile = Get-ChildItem $currentDirectory -Name | Where-Object { $_ -match '.sln$' }
+        Write-Host "Opening $solutionFile"
+    }
+    else 
+    {
+        $solutionFile = Get-ChildItem $currentDirectory -Name | Where-Object { $_ -match '.sln$' } | Select-Object -First 1
+        Write-Host "Multiple solution files found. Opening $solutionFile"
+    }    
+}
